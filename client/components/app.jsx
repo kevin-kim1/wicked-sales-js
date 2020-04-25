aae911c90aa9e9ea9b029ddf4b0eee0669fc00bd
Kevin Kim <kevinnhkim@live.com>
2020-04-25 16:43:12 -0700

    delete commented code

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,13 +3,6 @@ import Header from './header';
 import ProductList from './product-list';
 
 export default class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // this.state = {
-  //   message: null,
-  //   isLoading: true
-  // };
-  // }
 
   componentDidMount() {
     fetch('/api/health-check')
